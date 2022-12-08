061b82e24059cbf28885550c91a0bf2afed52aa3
Brieuc-Meyer <brieucme35@gmail.com>
2022-12-08 13:03:57 +0100

    Requette sql modifié , HAVING causait problèmes

--- models/modelAcceuil.js
+++ models/modelAcceuil.js
@@ -11,7 +11,7 @@ SELECT TIMESTAMPDIFF(MONTH, posologie_debut, posologie_fin)* Posologies.posologi
  * Requette pour le nbtotal de medicaments sur toutes les ordonnances et le stock
 
 
-SELECT Medicaments.*, SUM(TIMESTAMPDIFF(MONTH, posologie_debut, posologie_fin)* Posologies.posologie_nbboitesmois )as nbTotalBoitesUnePos
+SELECT Medicaments.*, SUM(TIMESTAMPDIFF(MONTH, posologie_debut, posologie_fin)* Posologies.posologie_nbboitesmois )as nbTotalBoitesNecessaires
 FROM Posologies, Medicaments
 WHERE Medicaments.medicament_id = Posologies.posologie_medicament_id
 GROUP BY Medicaments.medicament_nom;
@@ -21,14 +21,19 @@ GROUP BY Medicaments.medicament_nom;
 
 
 
-medocs jusquà mois actuel + 1
+somme des besoins en medoc pour chaque medoc pour le 1 er mois à venir 
 
-SELECT Medicaments.*, SUM(TIMESTAMPDIFF(MONTH, posologie_debut, posologie_fin)* Posologies.posologie_nbboitesmois )as nbTotalBoitesUnePos
-FROM Posologies, Medicaments
-WHERE Medicaments.medicament_id = Posologies.posologie_medicament_id
-AND posologie_fin <= (CURRENT_DATE() + INTERVAL 1 MONTH)
-GROUP BY Medicaments.medicament_nom;
- */
+Interval 2 MONTH sera pour le 2 eme mois a venir, ect...
+
+SELECT *, SUM(Posologies.posologie_nbboitesmois)
+FROM Medicaments 
+JOIN Posologies ON Medicaments.medicament_id = Posologies.posologie_medicament_id
+WHERE Posologies.posologie_fin >= DATE_ADD(curdate(), INTERVAL 15 MONTH)
+GROUP BY Medicaments.medicament_id;
 
 
 
+
+SELECT *, Medicaments.*, SUM(TIMESTAMPDIFF(MONTH, posologie_debut, posologie_fin)* Posologies.posologie_nbboitesmois )as nbTotalBoitesUnePos FROM Posologies, Medicaments WHERE Medicaments.medicament_id = Posologies.posologie_medicament_id GROUP BY Posologies.posologie_id ORDER BY `Medicaments`.`medicament_id` ASC
+
+*/
